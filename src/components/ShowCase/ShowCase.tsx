7ec7f1bad6386b849b202f9b7ac1a88147626327
witoldmetel <witoldmetel@gmail.com>
2020-05-07 08:56:59 +0200

    Add p5 library + implement example

--- src/components/ShowCase/ShowCase.tsx
+++ src/components/ShowCase/ShowCase.tsx
@@ -1,14 +1,42 @@
 import React, { Component } from 'react';
+import p5 from 'p5';
 
 import './ShowCase.scss';
 
 export class ShowCase extends Component {
+  private particlesRef: React.RefObject<HTMLInputElement>;
+
+  public constructor(props: any) {
+    super(props);
+
+    this.particlesRef = React.createRef();
+  }
+
+  public componentDidMount() {
+    const node = this.particlesRef.current;
+
+    new p5(this.sketch, node as HTMLInputElement);
+  }
+
+  private sketch = (sketch: p5) => {
+    const x = 100;
+    const y = 100;
+
+    sketch.setup = () => {
+      sketch.createCanvas(200, 200);
+    };
+
+    sketch.draw = () => {
+      sketch.background(0);
+      sketch.fill(255);
+      sketch.rect(x, y, 50, 50);
+    };
+  };
+
   render() {
     return (
       <div className="showcase">
-        <div className="gif-container">
-          <img src="https://thumbs.gfycat.com/ImpishUnequaledIchneumonfly-size_restricted.gif" />
-        </div>
+        <div className="particles-container" ref={this.particlesRef}></div>
         <div className="content">
           <h1>React + TypeScript + Parcel</h1>
           <h3>Simple Boilerplate</h3>
