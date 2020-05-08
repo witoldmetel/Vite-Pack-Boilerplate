c3ecb4639e32a24b0908dbf4bb4e3b1868993dec
witoldmetel <witoldmetel@gmail.com>
2020-05-08 14:29:11 +0200

    Update about section

--- src/components/Aboat/Aboat.tsx
+++ src/components/Aboat/Aboat.tsx
@@ -13,39 +13,25 @@ export class Aboat extends Component {
           <p>This is a minimal boilerplate example using React, TypeScript and Parcel along with Sass support</p>
         </div>
         <div className="section section-grid">
-          <div className="rellax" data-rellax-speed="1" data-rellax-xs-speed="3">
-            <i className="fas fa-video fa-3x secondary-text"></i>
-            <h2>
-              Watch<span className="secondary-text dot">.</span>
-            </h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore et dicta consectetur incidunt omnis nam
-              quis quidem nisi ipsa deserunt.
-            </p>
+          <div className="item react-section">
+            <h2>React</h2>
+            <p>React is a library for building composable user interfaces.</p>
           </div>
-          <div className="rellax" data-rellax-speed="4" data-rellax-xs-speed="3">
-            <i className="fas fa-users fa-3x secondary-text"></i>
-            <h2>
-              Share<span className="secondary-text dot">.</span>
-            </h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore et dicta consectetur incidunt omnis nam
-              quis quidem nisi ipsa deserunt.
-            </p>
+          <div className="item typescript-section">
+            <h2>TypeScript</h2>
+            <p>TypeScript is a typed superset of JavaScript that compiles to plain JavaScript.</p>
           </div>
-          <div className="rellax" data-rellax-speed="7" data-rellax-xs-speed="3">
-            <i className="fas fa-book fa-3x secondary-text"></i>
-            <h2>
-              Learn<span className="secondary-text dot">.</span>
-            </h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore et dicta consectetur incidunt omnis nam
-              quis quidem nisi ipsa deserunt.
-            </p>
+          <div className="item parcel-section">
+            <h2>Parcel</h2>
+            <p>Parcel is a blazing fast, zero configuration web application bundler</p>
+          </div>
+          <div className="item sass-section">
+            <h2>Sass</h2>
+            <p>Sass is the most mature, stable, and powerful professional grade CSS extension language in the world.</p>
           </div>
         </div>
         <div className="social-section">
-          <h1>Follow Me On Social Media</h1>
+          <h2>Follow Me On Social Media</h2>
           <a href="https://github.com/witoldmetel" target="_blank">
             <GitHubIcon />
           </a>
