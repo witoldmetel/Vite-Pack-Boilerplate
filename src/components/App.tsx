200e7238325050e15e604c293b539f5b33863d8d
Witold Mętel <31034370+witoldmetel@users.noreply.github.com>
2020-08-18 17:54:49 +0200

    Spike: Add firebase config setup (#6)

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import * as firebase from 'firebase/app';
 
 import { Aboat, ShowCase } from './index';
+import firebaseConfig from '../config/firebase';
 
 import './App.scss';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    firebase.initializeApp(firebaseConfig);
+  }
+
   render() {
     return (
       <React.Fragment>
---
+++ src/config/firebase.ts
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  /**
+   * Your firebase config
+   * Check documentation: https://firebase.google.com/docs/reference/
+   */
+};
+
+export default firebaseConfig;
