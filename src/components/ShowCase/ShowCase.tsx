4a201dad46f9abf5d4f16328babf9148bc5b7c57
witoldmetel <witoldmetel@gmail.com>
2020-08-19 17:30:56 +0200

    remove participles p5

--- src/components/ShowCase/ShowCase.tsx
+++ src/components/ShowCase/ShowCase.tsx
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
 
-import { ParticleComponent } from '../ParticleComponent/ParticleComponent';
-
 import './ShowCase.scss';
 
 export class ShowCase extends Component {
   render() {
     return (
       <div className="showcase">
-        <div className="particles-container">
-          <ParticleComponent />
-        </div>
         <div className="content">
           <h1>React + TypeScript + Parcel</h1>
           <h2>Simple Boilerplate</h2>
