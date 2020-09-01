01226ca7ff579cc7912b1f0ec50239f12e28cab3
witoldmetel <witoldmetel@gmail.com>
2020-09-01 19:28:14 +0200

    finish

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -24,38 +24,44 @@ export function MainPage() {
           href="https://reactjs.org/docs/getting-started.html"
           target="_blank"
           className="logo react-logo lax"
-          data-lax-opacity="(vh*0.7) 1, 0 0"
+          data-lax-opacity="(vh*0.8) 1, 0 0"
         />
         <a
           href="https://www.typescriptlang.org/docs"
           target="_blank"
           className="logo ts-logo lax"
-          data-lax-opacity="(vh*0.7) 1, 0 0"
+          data-lax-opacity="(vh*0.8) 1, 0 0"
         />
         <a
           href="https://parceljs.org/getting_started.html"
           target="_blank"
           className="logo parcel-logo lax"
-          data-lax-opacity="(vh*0.7) 1, 0 0"
+          data-lax-opacity="(vh*0.8) 1, 0 0"
         />
         <a
           href="https://sass-lang.com/documentation"
           target="_blank"
           className="logo sass-logo lax"
-          data-lax-opacity="(vh*0.7) 1, 0 0"
+          data-lax-opacity="(vh*0.8) 1, 0 0"
+        />
+        <a
+          href="https://tailwindcss.com/docs/installation"
+          target="_blank"
+          className="logo tailwind-logo lax"
+          data-lax-opacity="(vh*0.8) 1, 0 0"
         />
         <a
           href="https://firebase.google.com/docs"
           target="_blank"
           className="logo firebase-logo lax"
-          data-lax-opacity="(vh*0.7) 1, 0 0"
+          data-lax-opacity="(vh*0.8) 1, 0 0"
         />
         <div className="content">
           <div className="lax" data-lax-translate-y="0 0, vh -200">
             <h2 className="secondary-text">About</h2>
             <p>
               This is a minimal boilerplate using React and TypeScript along with Sass support. It&lsquo;s also possible
-              to config firebase. Good for small projects and prototyping.
+              to config firebase and use Tailwind low-level utility classes. Good for small projects and prototyping.
             </p>
           </div>
           <div className="lax" data-lax-translate-y="0 0, vh -200">
@@ -72,21 +78,21 @@ export function MainPage() {
       </section>
 
       <section className="section section-grid">
-        <div className="lax" data-lax-translate-y="0 0, vh -100">
+        <div className="lax" data-lax-translate-y="0 0, vh -50">
           <i className="fas fa-video fa-3x secondary-text"></i>
           <h2>
             React<span className="secondary-text dot">.</span>
           </h2>
           <p>React is a library for building composable user interfaces.</p>
         </div>
-        <div className="lax" data-lax-translate-y="0 0, vh -250">
+        <div className="lax" data-lax-translate-y="0 0, vh -200">
           <i className="fas fa-users fa-3x secondary-text"></i>
           <h2>
             TypeScript<span className="secondary-text dot">.</span>
           </h2>
           <p>TypeScript is a typed superset of JavaScript that compiles to plain JavaScript.</p>
         </div>
-        <div className="lax" data-lax-translate-y="0 0, vh -400">
+        <div className="lax" data-lax-translate-y="0 0, vh -350">
           <i className="fas fa-book fa-3x secondary-text"></i>
           <h2>
             Parcel<span className="secondary-text dot">.</span>
@@ -96,13 +102,15 @@ export function MainPage() {
       </section>
 
       <footer className="footer">
-        <a href="https://github.com/witoldmetel" target="_blank">
-          <GitHubIcon />
-        </a>
-        <a href="https://www.linkedin.com/in/witoldmetel" target="_blank">
-          <LinkedinIcon />
-        </a>
-        <p>
+        <div className="icons">
+          <a href="https://github.com/witoldmetel" target="_blank">
+            <GitHubIcon />
+          </a>
+          <a href="https://www.linkedin.com/in/witoldmetel" target="_blank">
+            <LinkedinIcon />
+          </a>
+        </div>
+        <p className="info-text">
           Effect created thanks to{' '}
           <a href="https://github.com/alexfoxy/lax.js" target="_blank">
             lax.js
