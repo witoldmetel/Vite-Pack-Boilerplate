9a031fd553a3bc17954998efc1db5c4357e05e74
witoldmetel <witoldmetel@gmail.com>
2020-09-01 17:42:47 +0200

    first effect

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/jsx-no-target-blank */
 import React from 'react';
-import { useLax, useLaxElement } from 'use-lax';
+import { useLax } from 'use-lax';
 
 import { GitHubIcon, LinkedinIcon } from '../../assets';
 
@@ -8,19 +8,6 @@ import './MainPage.scss';
 
 export function MainPage() {
   useLax();
-
-  const ref: React.MutableRefObject<any> = useLaxElement();
-
-  const element = (
-    <img
-      className="play"
-      src="https://i.ibb.co/TvdbMhQ/play-button.png"
-      ref={ref}
-      data-lax-preset="rightToLeft fadeIn"
-      data-lax-opacity="0 1"
-      data-lax-translate-x="vh 0"
-    />
-  );
 
   return (
     <div className="showcase">
@@ -33,22 +20,22 @@ export function MainPage() {
       </section>
 
       <section className="section section-main">
-        {element}
+        <div className="react-logo lax" data-lax-translate-x="0 0, vh -1000" data-lax-opacity="(vh*0.8) 1, 0 0" />
         <div className="content">
-          <div>
+          <div className="lax" data-lax-translate-y="0 0, vh -200">
             <h2 className="secondary-text">About</h2>
             <p>
               This is a minimal boilerplate using React and TypeScript along with Sass support. It&lsquo;s also possible
               to config firebase. Good for small projects and prototyping.
             </p>
           </div>
-          <div>
+          <div className="lax" data-lax-translate-y="0 0, vh -200">
             <h2 className="secondary-text">Questions</h2>
             <p>
-              In case of issues and questions, feel free to add the question in
+              In case of issues and questions, feel free to add the question in{' '}
               <a href="https://github.com/witoldmetel/TS-Parcel-Boilerplate/issues" target="_blank">
                 Issues
-              </a>
+              </a>{' '}
               page or write me directly on GitHub
             </p>
           </div>
@@ -56,21 +43,21 @@ export function MainPage() {
       </section>
 
       <section className="section section-grid">
-        <div>
+        <div className="lax" data-lax-translate-y="0 0, vh -100">
           <i className="fas fa-video fa-3x secondary-text"></i>
           <h2>
             React<span className="secondary-text dot">.</span>
           </h2>
           <p>React is a library for building composable user interfaces.</p>
         </div>
-        <div>
+        <div className="lax" data-lax-translate-y="0 0, vh -250">
           <i className="fas fa-users fa-3x secondary-text"></i>
           <h2>
             TypeScript<span className="secondary-text dot">.</span>
           </h2>
           <p>TypeScript is a typed superset of JavaScript that compiles to plain JavaScript.</p>
         </div>
-        <div>
+        <div className="lax" data-lax-translate-y="0 0, vh -400">
           <i className="fas fa-book fa-3x secondary-text"></i>
           <h2>
             Parcel<span className="secondary-text dot">.</span>
@@ -86,6 +73,12 @@ export function MainPage() {
         <a href="https://www.linkedin.com/in/witoldmetel" target="_blank">
           <LinkedinIcon />
         </a>
+        <p>
+          Effect created thanks to{' '}
+          <a href="https://github.com/alexfoxy/lax.js" target="_blank">
+            lax.js
+          </a>
+        </p>
       </footer>
     </div>
   );
