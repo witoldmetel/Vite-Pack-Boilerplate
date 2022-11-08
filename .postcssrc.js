19334ad1698042be316a45c0c3edad9a1a824b23
witoldmetel <witoldmetel@gmail.com>
2022-11-08 18:38:21 +0100

    working postcss

--- .postcssrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  parser: 'sugarss',
-  map: false,
-  plugins: {
-    'postcss-plugin': {},
-    autoprefixer: {},
-  },
-};
--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,13 @@
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vite';
-import autoprefixer from "autoprefixer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   css: {
     postcss: {
-      plugins: [autoprefixer],
+      plugins: [require('autoprefixer'), require('postcss-import'), require('postcss-nested')],
     },
   },
   test: {
