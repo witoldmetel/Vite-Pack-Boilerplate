d8eae05c6a7060468f19d6dd6b4b1f682b4d5584
witoldmetel <witoldmetel@gmail.com>
2022-11-11 13:53:12 +0100

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -13,5 +13,5 @@ export default defineConfig({
       plugins: [require('autoprefixer'), require('postcss-import'), require('postcss-nested')],
     },
   },
-  base: '/Vite-Pack-Boilerplate/',
+  base: process.env.NODE_ENV === 'production' ? '/Vite-Pack-Boilerplate/' : './',
 });
